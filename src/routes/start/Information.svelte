4edbd21afbfa5fa2163bbfaca519e1aa0de5f398
JorimJoram <wkdgyfla97@gmail.com>
2024-10-08 17:38:03 +0900

    change image

--- src/routes/start/Information.svelte
+++ src/routes/start/Information.svelte
@@ -6,14 +6,14 @@
 <div class="information_select">
     <p class="information_title">성별</p>
     <select name="gender" id="start_gender" class="form-select" bind:value={gender}>
-        <option value="남성">남성</option>
+        <option value="남성" selected>남성</option>
         <option value="여성">여성</option>
     </select>
 </div>
 <div class="information_select">
     <p class="information_title">나이(대)</p>
     <select name="gender" id="start_gender" class="form-select" bind:value={age}>
-        <option value="10">10대</option>
+        <option value="10" selected>10대</option>
         <option value="20">20대</option>
         <option value="30">30대</option>
         <option value="40">40대</option>
